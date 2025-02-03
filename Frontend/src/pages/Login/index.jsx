776a570ee87f9704de4fbf84d361e882faac4f18
MilashaThathsarani <milashathathsarni99@gmail.com>
2025-02-03 22:37:40 +0530

    add login-index.jsx and style.js

---
+++ Frontend/src/pages/Login/index.jsx
@@ -0,0 +1,18 @@
+import React, {Components} from "react";
+import { styleSheet } from "./style";
+import {withStyles} from 
+
+class Login extends Components {
+    constructor (props){
+        super (props);
+    }
+
+    render(){
+        const { classes } = this.props;
+        return{
+         
+        }
+    }
+}
+
+export default withStyles (styleSheet)(Login)
